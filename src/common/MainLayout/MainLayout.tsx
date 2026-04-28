9a11ec9d202a381c4ee87cf661791fea9bad2119
Om Harika Peddapudi <harika.peddapudi@valuemomentum.com>
2026-04-28 10:57:04 +0530

    Added digital documents updates

--- src/common/MainLayout/MainLayout.tsx
+++ src/common/MainLayout/MainLayout.tsx
@@ -14,14 +14,14 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className='min-h-screen w-full flex flex-col'>
+    <div className='h-screen w-full flex flex-col'>
       <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
 
-      <div className='flex flex-1'>
+      <div className='flex flex-1 overflow-hidden'>
         <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
-        <main className='flex-1 md:ml-0 flex flex-col'>
-          <div className='flex-1'>{children}</div>
+        <main className='flex-1 md:ml-0 flex flex-col overflow-hidden'>
+          <div className='flex-1 overflow-y-auto'>{children}</div>
           <Footer />
         </main>
       </div>
